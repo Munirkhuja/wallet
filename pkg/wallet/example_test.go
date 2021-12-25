e51e1a1af0cce7eef8f2a6d376efd35408c15fad
munirhuja1357@gmail.com <bit314151>
2021-12-25 19:57:33 +0500

    v1.1.0

--- pkg/wallet/example_test.go
+++ pkg/wallet/example_test.go
@@ -3,11 +3,44 @@ package wallet
 import (
 	"reflect"
 	"testing"
-	"github.com/Munirkhuja/wallet/pkg/types"
-)
 
+	"github.com/Munirkhuja/wallet/pkg/types"
+	"github.com/google/uuid"
+)
+func TestService_Reject(t *testing.T) {
+	uuidTest:=uuid.New().String()
+	accounts:=[]*types.Account{
+		{ID: 1, Balance: 5_00,Phone: "992928882211"},
+		{ID: 2, Balance: 50_00,Phone: "992928882212"},
+		{ID: 3, Balance: 10_00,Phone: "992928882213"},
+		{ID: 4, Balance: 16_00,Phone: "992928882214"},
+	}
+	payments:=[]*types.Payment{
+		{ID: uuidTest,AccountID: 2,Amount: 30_00, Status: types.PaymentStatusInProgress},
+		{ID: uuid.New().String(),AccountID: 2,Amount: 3_00, Status: types.PaymentStatusInProgress},
+		{ID: uuid.New().String(),AccountID: 1,Amount: 10_00, Status: types.PaymentStatusOk},
+		{ID: uuid.New().String(),AccountID: 2,Amount: 30_00, Status: types.PaymentStatusInProgress},
+		{ID: uuid.New().String(),AccountID: 4,Amount: 15_00, Status: types.PaymentStatusInProgress},
+		{ID: uuid.New().String(),AccountID: 2,Amount: 90_00, Status: types.PaymentStatusFail},
+		{ID: uuid.New().String(),AccountID: 1,Amount: 1_000_00, Status: types.PaymentStatusOk},
+	}	
+	service:=Service{nextAccountId: 5,accounts: accounts,payments: payments}
+	expected := &types.Payment{ID: uuidTest,AccountID: 2,Amount: 30_00, Status: types.PaymentStatusFail}
+	result,err := service.Reject(uuidTest)
+	if err==ErrAccountNotFound {
+		t.Error(err)
+		return
+	}
+	if err==ErrPaymentNotFound {
+		t.Error(err)
+		return
+	}
+	if !reflect.DeepEqual(result,expected) {
+		t.Errorf("invalid result, expected: %v actual: %v",expected,result)
+	}
+}
 func TestService_FindAccountByID(t *testing.T) {
-	accounts:=[]types.Account{
+	accounts:=[]*types.Account{
 		{ID: 1, Balance: 5_00,Phone: "992928882211"},
 		{ID: 2, Balance: 50_00,Phone: "992928882212"},
 		{ID: 3, Balance: 10_00,Phone: "992928882213"},
--- pkg/wallet/service.go
+++ pkg/wallet/service.go
@@ -4,19 +4,22 @@ import (
 	"errors"
 
 	"github.com/Munirkhuja/wallet/pkg/types"
+	"github.com/google/uuid"
 )
 var ErrAccountNotFound=errors.New("account not found")
 var ErrPhoneRegistered=errors.New("phone already registered")
 var ErrAmountMustBePositive= errors.New("amount must be greater then zero")
+var ErrPaymentNotFound=errors.New("payment not found")
+var ErrNotEnoughBalance=errors.New("not enough balance")
 type Service struct{
 	nextAccountId int64
-	accounts []types.Account
-	payments []types.Payment
+	accounts []*types.Account
+	payments []*types.Payment
 }
 func (s *Service) FindAccountByID(accountID int64) (*types.Account, error)  {
 	for _, account :=range s.accounts{
 		if account.ID==accountID {
-			return &account,nil
+			return account,nil
 		}
 	}
 	return nil,ErrAccountNotFound
@@ -33,6 +36,57 @@ func (s *Service) RegisterAccount(phone types.Phone)(*types.Account,error){
 		Phone: phone,
 		Balance: 0,
 	}
-	s.accounts=append(s.accounts, *account)
+	s.accounts=append(s.accounts, account)
 	return account,nil
+}
+func(s *Service)Pay(accountID int64, amount types.Money, category types.PaymentCategory)(*types.Payment,error){
+	if amount<=0 {
+		return nil,ErrAmountMustBePositive
+	}
+	var account *types.Account
+	for _, acc:=range s.accounts{
+		if acc.ID==accountID {
+			account=acc
+			break
+		}
+	}
+	if account==nil {
+		return nil,ErrAccountNotFound
+	}
+	if account.Balance<amount {
+		return nil,ErrNotEnoughBalance
+	}
+	account.Balance-=amount
+	paymentID:=uuid.New().String()
+	payment:=&types.Payment{
+		ID: paymentID,
+		AccountID: accountID,
+		Amount: amount,
+		Category: category,
+		Status: types.PaymentStatusInProgress,
+	}
+	s.payments=append(s.payments, payment)
+	return payment,nil
+}
+func (s *Service)Reject(paymentID string) (*types.Payment, error) {
+	payment,err:=s.FindPaymentByID(paymentID)
+	if err==nil {
+		account,err:=s.FindAccountByID(payment.AccountID)
+		if account==nil {
+			return nil,err
+		}
+		if payment.Status!=types.PaymentStatusFail {
+			account.Balance+=payment.Amount
+			payment.Status=types.PaymentStatusFail		
+		}
+	}
+	return payment,err
+}
+func (s *Service)FindPaymentByID(paymentID string) (*types.Payment, error) {
+	for _, payment :=range s.payments{
+		if paymentID==payment.ID {
+			return payment,nil
+		}
+	}
+	return nil,ErrPaymentNotFound
 }
